6eb22ae6c009e6305c3ae4ef80c5bd2836c3904b
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-10-30 15:32:48 +0200

    Added gen_user_vector, content_base.py

--- train/content_base.py
+++ train/content_base.py
@@ -6,6 +6,7 @@ class content_base():
         self.user_behavior = self.convert_tf_tensor(user_behavior)
         self.anime_features = self.convert_tf_tensor(anime_features)
         self.curr_user = user_id
+        self.anime_ids = None
 
     def gen_user_anime(self):
         """returns a matrix for each row represent the rating of the movie id to that specific user"""
@@ -19,18 +20,24 @@ class content_base():
 
     def gen_anime_feats(self):
         #TODO optimize this code
-        self.gen_user_anime()
+        if not self.anime_ids:
+            self.gen_user_anime()
+
         anime_id_col = tf.squeeze(tf.gather(self.anime_features, [0], axis=1))
         # Getting the index of rows that has our anime_id
         anime_row_index = tf.squeeze(tf.where(
             [True if indx in self.anime_ids else False for indx in anime_id_col]))
         # Getting the rows of our anime
-        anime_feats = tf.squeeze(tf.gather(self.anime_features, anime_row_index, axis=0))
+        anime_feats = tf.squeeze(
+            tf.gather(self.anime_features, anime_row_index, axis=0))
         # Genre is column indx 2
         anime_feats = tf.squeeze(tf.gather(anime_feats, [2], axis=1))
-        return anime_feats
-    #TODO a fucntion to generate user vector
-        
+        return anime_feats #tf.transpose(anime_feats)
+
+    def gen_user_vector(self):
+        user_anime = self.gen_user_anime()
+        anime_feats = self.gen_anime_feats
+        return tf.matmul(anime_feats, user_anime)
     #TODO a function to generate movies rating
 
     #TODO a function to get the top predicted rating
@@ -47,6 +54,7 @@ class content_base():
 
 user = [[1, 3, 5, 6, 4, 7], [2, 3, 5, 6, 7, 8],
         [1, 1, 3, 6, 7, 8], [1, 6, 10, 6, 7, 8]]
-movie = [[3, 6, [0,1], 7, 8], [10, 6, [2,3], 7, 8], [1, 6, [3,6], 7, 8], [6, 6, [7,4], 7, 8]]
+movie = [[3, 6, 0, 7, 8], [10, 6, 3, 7, 8], [1, 6, 3, 7, 8], [6, 6, 7, 7, 8]]
+#[[3, 6, [0,1], 7, 8], [10, 6, [2,3], 7, 8], [1, 6, [3,6], 7, 8], [6, 6, [7,4], 7, 8]]
 con = content_base(user, movie, 1)
 print(f"Our value: {con.gen_anime_feats()}")
