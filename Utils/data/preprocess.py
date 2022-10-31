8bc34c64c0a7fe48722f876e98d7d20ef9a2ceaf
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-10-31 16:46:09 +0200

    Added preprocess class for content based recom system

--- Utils/data/preprocess.py
+++ Utils/data/preprocess.py
@@ -1,9 +1,10 @@
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import numpy as np
 import pickle
-import config_preprocess
+from Utils.data import config_preprocess
 
 FIRST_PREPROCESS = config_preprocess.FIRST_PREPROCESS
+PREPROCESS_MAP = config_preprocess.PREPROCESS_MAP
 
 
 def label_encoder(data, name, first_preprocess=FIRST_PREPROCESS):
@@ -32,10 +33,12 @@ def onehot_encoder(data, name, first_preprocess=FIRST_PREPROCESS):
     data = np.array(data).reshape(-1, 1)
     if first_preprocess:
         encoder = OneHotEncoder(sparse=False)
-        pickle.dump(encoder, open(f'{name}.pkl', 'wb'))
         encoder.fit(data)
+        pickle.dump(encoder, open(f'{name}.pkl', 'wb'))
+
     else:
         encoder = pickle.load(open(f'{name}.pkl', 'rb'))
+        print(f"loaded: {name}")
     return encoder.transform(data)
 
 
@@ -66,7 +69,7 @@ def extract_years_num(data):
 
 
 class label_onehot_encode():
-    def __init__(self, input,name, first_preprocess=FIRST_PREPROCESS):
+    def __init__(self, input, name, first_preprocess=FIRST_PREPROCESS):
         """This class made because I belive we should get more informations about Genres, splite them and combine them,
          and not to lose some informations about it
          args:
@@ -86,6 +89,7 @@ class label_onehot_encode():
             pickle.dump(self.data_dec, open(f'{name}.pkl', 'wb'))
         else:
             self.data_dec = pickle.load(open(f'{name}.pkl', 'rb'))
+            self.num_uniq = len(self.data_dec)
 
     def split_words(self, input):
         return input.replace(" ", "").split(",")
@@ -115,3 +119,39 @@ class label_onehot_encode():
                 if not label in self.data_dec:
                     self.data_dec[label] = self.num_uniq
                     self.num_uniq += 1
+
+
+class preprocessor_anime_data():
+    def __init__(self, data, pre_process_dict=PREPROCESS_MAP):
+        """args:
+            data: pandas data
+            pre_process_dict: dictionary defines what happens to each column."""
+        self.data = data
+        self.pre_process_dict = pre_process_dict
+        self.process()
+
+    def get_transformed_data(self):
+        return self.data
+
+    def excute_function(self, column, function_name, name):
+        processed_data = None
+        if function_name == "label_onehot_encode":
+            processed_data = label_onehot_encode(column, name).transform_data()
+        elif function_name == "label_encoder":
+            processed_data = label_encoder(column, name)
+        elif function_name == "extract_years_num":
+            processed_data = extract_years_num(column)
+        elif function_name == "onehot_encoder":
+            processed_data = onehot_encoder(column, name)
+        elif function_name == "conv_dur_min":
+            processed_data = conv_dur_min(column)
+        return processed_data
+
+    def process(self):
+        preprocess_map = self.pre_process_dict
+        for i, feature in enumerate(preprocess_map["anime"]["Process"]):
+            function = preprocess_map["anime"]["Process"][feature]
+            self.data[feature] = self.excute_function(
+                self.data[feature], function_name=function, name=feature)
+        self.data.drop(preprocess_map["anime"]["Drop"], axis=1, inplace=True)
+        return self.data
