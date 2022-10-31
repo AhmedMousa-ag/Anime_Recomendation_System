8cdc48b17575de6d714f573ded966279ff7fc241
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-10-31 16:46:46 +0200

    Added dict for prepro config

--- Utils/data/config_preprocess.py
+++ Utils/data/config_preprocess.py
@@ -1,1 +1,20 @@
-FIRST_PREPROCESS = False
+FIRST_PREPROCESS = True
+
+# Will one hot encocde Genres after splitting it and Type as well,
+# Label encode producers, bucktize Aired per year, Label encode studio,
+# one hot encode Source, Convert Duration to minutes, One Hot encode Rating.
+PREPROCESS_MAP = {"anime":
+                            {"Drop":
+                                    ["Name","English name", "Japanese name","Premiered", "Licensors"],
+                            "Process": {"Genres": "label_onehot_encode",
+                                        "Type": "onehot_encoder",
+                                        "Producers": "label_encoder",
+                                        "Aired": "extract_years_num",
+                                        "Studios": "label_encoder",
+                                        "Source": "onehot_encoder",
+                                        "Duration": "conv_dur_min",
+                                        "Rating": "onehot_encoder"}}
+                                           }
+
+
+
